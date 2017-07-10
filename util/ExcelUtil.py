bd090437cd077ab54281afb5a49d7e5bcd588fba
derek.zhang <923770317@qq.com>
2017-07-10 17:09:59 +0800

    update

--- util/ExcelUtil.py
+++ util/ExcelUtil.py
@@ -1,9 +1,9 @@
  #-*- coding: utf-8 -*-
-
-
+import Constants
 import xlrd
 import xlwt
 from xlutils.copy import copy
+
 
 class ExcelUtil():
     excelSheet = None
@@ -20,6 +20,7 @@ class ExcelUtil():
         except Exception,e:
             print str(e)
 
+
     @staticmethod
     def getCellData(rowNum,colNum):
         try:
@@ -28,12 +29,14 @@ class ExcelUtil():
         except Exception,e:
             print str(e)
 
+
     @staticmethod
     def getLastRowNum():
         try:
             return ExcelUtil.excelBook.nrows
         except Exception,e:
             print str(e)
+
 
     @staticmethod
     def setCellData(rowNum,colNum,result,path):
@@ -46,6 +49,7 @@ class ExcelUtil():
 
         except Exception,e:
             print str(e)
+
 
     @staticmethod
     def gonext():
@@ -60,6 +64,7 @@ class ExcelUtil():
            ExcelUtil.curRowNo += 1
        return r
 
+
     @staticmethod
     def getValue():
         r = []
@@ -71,7 +76,6 @@ class ExcelUtil():
             print str(e)
 
 
-
     @staticmethod
     def hasNext():
         if ExcelUtil.excelBook.nrows == 0 or ExcelUtil.excelBook.nrows <= ExcelUtil.curRowNo:
@@ -79,15 +83,56 @@ class ExcelUtil():
         else:
             return True
 
+
+    #获取指定sheet 中的总行数
+    @staticmethod
+    def getRowCount(sheetName):
+        try:
+            number = ExcelUtil.getLastRowNum()
+            return number
+        except Exception,e:
+            print str(e)
+
+
+    #在指定sheet中，获取第一次包含指定测试用例序号文字的行号
+    @staticmethod
+    def getFirstRowContainsTestCaseId(sheentName,testCaseName,colNum):
+        try:
+            flag = False
+            rowNum = ExcelUtil.getRowCount(sheentName)
+            for i in range(1,rowNum):
+                if str(ExcelUtil.getCellData(i,colNum)).lower() == testCaseName:
+                    flag = True
+                    return i
+            if(flag == False):
+                raise Exception("么有")
+        except Exception,e:
+            print str(e)
+
+
+    #获取指定Sheet中某个测试用例步骤的个数
+    @staticmethod
+    def getTestCaseLastStepRow(sheetName,testCaseId,testCaseStartRowNum):
+        try:
+            rowNum = ExcelUtil.getRowCount(sheetName)
+            for i in range(testCaseStartRowNum,rowNum):
+                if not str(ExcelUtil.getCellData(i,Constants.Constants.Col_TestCaseID)).lower() == testCaseId :
+                    return i
+            #如果是 最后一行结束
+            return rowNum + 1
+        except Exception,e:
+            print str(e)
+
+
 if __name__ == "__main__":
     # codedetect = chardet.detect("百度测试数据.xls")["encoding"]
     # print codedetect
     # ustring = unicode("百度测试数据.xls", codedetect)
-    ExcelUtil.setExcelFile("1111.xls","1111.xls")
+    ExcelUtil.setExcelFile(Constants.Constants.path_excelFile,Constants.Constants.Sheet_TestSteps)
     # # ExcelUtil.setCellData(5,1,'sema')
     # # print ExcelUtil.excelBook.row_values(0)
     # # ExcelUtil.gonext()
-    print ExcelUtil.getCellData(1,0)
+    print ExcelUtil.getFirstRowContainsTestCaseId("testsuite","baidu02",0)
 
 
 
